b728535fdb567bcab7e6e11ff92219f638dbdf4b
Jacob <maimaisatt@gmail.com>
2025-03-13 14:44:57 +0800

    Modify StockOrderHandler

--- Core/Service/StockTraderService.cs
+++ Core/Service/StockTraderService.cs
@@ -238,7 +238,7 @@ namespace Core.Service
         private void StockOrderHandler(string strResult)
         {
             string[] resultArray = strResult.Split(',');
-            if (!DateTime.TryParse(resultArray[5], out DateTime orderTime))
+            if (string.IsNullOrEmpty(resultArray[4].Trim()) || !DateTime.TryParse(resultArray[5], out DateTime orderTime))
             {
                 _logger.Error($"SendStockOrder error. Error message: {resultArray[resultArray.Length - 2]}, {resultArray[resultArray.Length - 1]}");
                 _hasStockOrder = false;
